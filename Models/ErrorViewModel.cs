2180652b42a15f0c0ec916ba1e95c167b861528f
Sibonelo <Sibonelo@LAPTOP-UP3A6LJC>
2025-07-01 21:23:28 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CareDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
