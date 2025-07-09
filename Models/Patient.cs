6dbf0411cc8bf52afc02d1a5abf5c415dd7930c6
Sbonelo Vimba <s225118440@mandela.ac.za>
2025-07-09 20:19:43 +0200

    removed initializations in the patient's class

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -24,23 +24,24 @@ namespace CareDev.Models
         [Display(Name="Phone Number")]
         [RegularExpression(@"^0\d{9}$", ErrorMessage = "Phone number must be 10 digits and start with 0.")]
         [StringLength(10,MinimumLength =10, ErrorMessage = "Phone number must be 10 digits long.")]
-        public string PhoneNumber { get; set; } //= string.Empty; // Initialize to avoid null reference issues
+        public string PhoneNumber { get; set; } = string.Empty; // Initialize to avoid null reference issues
 
         //Foreign Keys
         public int GenderOptionId { get; set; }
-        public int MedicationId { get; set; }
-        public int AllergyId { get; set; }
-        public int ChronicConditionId { get; set; }
+        public int? MedicationId { get; set; }
+        public int? AllergyId { get; set; }
+        public int? ChronicConditionId { get; set; }
 
 
         //Navigation Properties 
-        public virtual GenderOption Gender { get; set; } // = new GenderOption(); // Initialize to avoid null reference issues
-        public virtual Medication Medication { get; set; }// = new Medication(); // Initialize to avoid null reference issues
-        public virtual Allergy Allergy { get; set; } //= new Allergy(); // Initialize to avoid null reference issues
-        public virtual ChronicCondition ChronicCondition { get; set; }//= new ChronicCondition();
+        public virtual GenderOption Gender { get; set; } 
+        public virtual Medication Medication { get; set; } 
+        public virtual Allergy Allergy { get; set; } 
+        public  ChronicCondition ChronicCondition { get; set; } 
 
         //Relationships
 
+        public ICollection<Admission> Admissions { get; set; } = new List<Admission>();
 
 
 
