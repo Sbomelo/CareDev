529ceec8d129d24729679ba7b73f3a6b04f0ce51
Sbonelo Vimba <s225118440@mandela.ac.za>
2025-09-30 23:27:14 +0200

    New seeded Medication Data migration

--- Data/Migrations/20250930212349_SeededNewMedicationData.Designer.cs
+++ Data/Migrations/20250930212349_SeededNewMedicationData.Designer.cs
@@ -4,6 +4,7 @@ using CareDev.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CareDev.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250930212349_SeededNewMedicationData")]
+    partial class SeededNewMedicationData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20250930212349_SeededNewMedicationData.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareDev.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededNewMedicationData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 1,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "None", "N/A" });
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 2,
+                column: "Name",
+                value: "Paracetamol");
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 3,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "Ibuprofen", "PRN" });
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 4,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "Amoxicillin", "Scheduled" });
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 5,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "Aspirin", "Schedules" });
+
+            migrationBuilder.InsertData(
+                table: "Medications",
+                columns: new[] { "MedicationId", "EmployeeId", "Name", "PatientId", "Schedule", "UsageNotes" },
+                values: new object[] { 6, null, "Metformin", null, "PRN", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 1,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "Paracetamol", "PRN" });
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 2,
+                column: "Name",
+                value: "Ibuprofen");
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 3,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "Amoxicillin", "Scheduled" });
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 4,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "Aspirin", "Schedules" });
+
+            migrationBuilder.UpdateData(
+                table: "Medications",
+                keyColumn: "MedicationId",
+                keyValue: 5,
+                columns: new[] { "Name", "Schedule" },
+                values: new object[] { "Metformin", "PRN" });
+        }
+    }
+}
