1eb5fd8b1e45c68446ce31fc4853ef3aa0b6e496
Sbonelo Vimba <s225118440@mandela.ac.za>
2025-10-05 14:07:14 +0200

    Updated the Admission Table properties, and the Views

--- Models/Admission.cs
+++ Models/Admission.cs
@@ -1,58 +1,4 @@
-﻿//using System.ComponentModel.DataAnnotations;
-
-//namespace CareDev.Models
-//{
-//    public class Admission
-//    {
-//        [Key]
-//        public int AdmissionId { get; set; }
-
-//        [Required]
-//        [DataType(DataType.Date)]
-//        public DateTime AdmissionDate { get; set; } = DateTime.Now;
-
-//        [DataType(DataType.Date)]
-//        public DateTime? DischargeDate { get; set; } // Nullable to allow for ongoing admissions
-
-//        [Required]
-//        [StringLength(500, ErrorMessage = "Admission Reason cannot exceed 500 characters.")]
-//        public required string AdmissionReason { get; set; } = string.Empty;
-
-//        //Foreign Keys
-//        [Required]
-//        [Display (Name ="Patient Name")]
-//        public int PatientId { get; set; }  
-
-//        [Required]
-//        [Display (Name = "Doctor")]
-//        public int? EmployeeId { get; set; }
-
-//        [Required]
-//        [Display (Name = "Ward")]
-//        public int WardId { get; set; }
-
-//        [Required]
-//        [Display (Name = "Bed Number")]
-//        public int BedId { get; set; }
-
-//        [Required]
-//        [Display (Name = "Room Type")]
-//        public string? RoomId { get; set; }
-
-//        // Navigation Proproperties 
-//        public Patient Patient { get; set; } 
-//        public Employee Employee { get; set; } 
-//        public Ward Ward { get; set; } 
-//        public Bed Bed { get; set; } 
-//        public RoomType Room { get; set; } 
-
-
-
-
-//    }
-//}
-
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -65,15 +11,18 @@ namespace CareDev.Models
 
         // The patient being admitted (required)
         [Required]
+        [Display (Name="Patient")]
         public int PatientId { get; set; }
         public Patient Patient { get; set; } = null!;
 
         // Ward where patient is admitted (required)
         [Required]
+        [Display (Name="Ward")]
         public int WardId { get; set; }
         public Ward Ward { get; set; } = null!;
 
         // Bed can be assigned later, so nullable
+        [Display (Name="Bed")]
         public int? BedId { get; set; }
         public Bed? Bed { get; set; }
 
@@ -81,6 +30,7 @@ namespace CareDev.Models
         public int? DoctorId { get; set; }
         public Doctor? Doctor { get; set; }
 
+        [Display(Name="Employee")]
         public int? EmployeeId { get; set; }
         public Employee? Employee { get; set; }
 
