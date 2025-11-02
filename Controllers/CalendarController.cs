414ba4f680b7df6cc7619fb8d60bf0b46f130bf7
Sibonelo Vimba <s225118440@mandela.ac.za>
2025-11-03 00:23:30 +0200

    fixed IIS Server appointment issues

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,25 +1,51 @@
 ﻿using CareDev.Services.IService;
 using Microsoft.AspNetCore.Mvc;
+using CareDev.Data;
+using Microsoft.EntityFrameworkCore;
+using static CareDev.Models.ApplicationUser;
 
 namespace CareDev.Controllers
 {
     public class CalendarController : Controller
     {
         private readonly IAppointmentService _appointmentService;
-        public CalendarController(IAppointmentService appointmentService) { _appointmentService = appointmentService; }
+        private readonly ApplicationDbContext _context;
+        public CalendarController(IAppointmentService appointmentService, ApplicationDbContext context) 
+        { 
+            _appointmentService = appointmentService;
+            _context = context;
+        }
 
         public IActionResult Index() => View();
 
         public async Task<IActionResult> GetEvents()
         {
-            var events = (await _appointmentService.GetAllAppointmentsAsync())
-                .Select(a => new {
-                    id = a.Id,
-                    title = $"{a.Patient?.Name ?? a.PatientId}",
-                    start = a.Start.ToString("s"),
-                    end = a.End.ToString("s"),
-                    status = a.Status.ToString()
-                });
+            //var events = (await _appointmentService.GetAllAppointmentsAsync())
+            //    .Select(a => new {
+            //        id = a.Id,
+            //        title = $"{a.Patient?.Name ?? a.PatientId}",
+            //        start = a.Start.ToString("s"),
+            //        end = a.End.ToString("s"),
+            //        status = a.Status.ToString()
+            //    });
+
+            //return Json(events);
+
+
+            var events = await _context.Appointments
+        .AsNoTracking()
+        .Include(a => a.Patient)
+        .Include(a => a.Doctor)
+        .Where(a => a.Status != AppointmentStatus.Cancelled)
+        .OrderBy(a => a.Start)
+        .Select(a => new {
+            id = a.Id,
+            title = a.Patient.Name ?? a.Patient.UserName,
+            start = a.Start.ToString("s"),
+            end = a.End.ToString("s"),
+            status = a.Status.ToString()
+        })
+        .ToListAsync();
 
             return Json(events);
         }
